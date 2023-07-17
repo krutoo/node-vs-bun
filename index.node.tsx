a8bce9bf3cd4d7e3bc1fdf071f16eccc59427ac8
krutoo <dima.krutoo@yandex.ru>
2023-07-17 17:51:20 +0500

    fixes

--- index.node.tsx
+++ index.node.tsx
@@ -1,6 +1,11 @@
 import { renderToString, renderToPipeableStream } from "react-dom/server";
 import express from "express";
+import dotenv from "dotenv";
 import { App } from "./common";
+
+dotenv.config({
+  path: "./.env.local",
+});
 
 const app = express();
 
